5da8ecf60d30ce4a1ed4e613f968e6b2f2ae3817
TSRGSG <32558834+TSRGSG@users.noreply.github.com>
2018-04-05 15:20:51 -0400

    Cleaning

--- JS/index.js
+++ JS/index.js
@@ -1,19 +1,13 @@
 function calculate() {
 
-    var balance = document.getElementById("balance").value;
-    var monthly = document.getElementById("monthly").value;
-    var interest = document.getElementById("interest").value;
-    var compound = document.getElementById("compound").value;
-    var year = document.getElementById("year").value;
-
-    var P = parseInt(balance);
-    var R = parseInt(interest);
-    var n = parseInt(compound);
-    var r = R / 100;
-    var t = parseInt(year);
-
-    m = monthly;
-    var A = parseInt(P);
+    var balance = parseInt(document.getElementById("balance").value);
+    var monthly = parseIntdocument.getElementById("monthly").value);
+    var interest = parseInt(document.getElementById("interest").value);
+    var compound = parseInt(document.getElementById("compound").value);
+    var year = parseInt(document.getElementById("year").value);
+    
+    var r = interest / 100;    
+    var A = balance;
 	
 	var BottomLabel = ["0"];;
 	var ActualAmount = ["0"];
@@ -22,9 +16,9 @@ function calculate() {
 	$(mainList).empty();
 
 	//Checking for Yearly, Semiannually, Quarterly, & Monthly.
-	if (parseInt(n) == 12){
+    if (compound == 12){
 		//Main Loop (Read Carefully)
-    	for (count = 1; count != t + 1; count++) {
+        for (count = 1; count != year + 1; count++) {
         	A = (A * r) + parseInt(A);
 			A += parseInt(m) * 12;
 		
@@ -35,8 +29,8 @@ function calculate() {
 			BottomLabel.push("Year " + count);
 			ActualAmount.push(A);
     	}
-	} else if (n == 6) {
-		for (count = 1; count != (t * 2)+ 1; count++) {
+    } else if (compound == 6) {
+        for (count = 1; count != (year * 2)+ 1; count++) {
         	A = (A * (r/2)) + parseInt(A);
 			A += parseInt(m) * 6;
 		
@@ -47,8 +41,8 @@ function calculate() {
 			BottomLabel.push("Semiannually " + count);
 			ActualAmount.push(A);
     	}	
-	} else if (n == 3) {
-		for (count = 1; count != (t * 4) + 1; count++) {
+    } else if (compound == 3) {
+        for (count = 1; count != (year * 4) + 1; count++) {
         	A = (A * (r/4)) + parseInt(A);
 			A += parseInt(m) * 3;
 		
@@ -59,8 +53,8 @@ function calculate() {
 			BottomLabel.push("Quarter " + count);
 			ActualAmount.push(A);
     	}
-	} else if (n == 1) {
-		for (count = 1; count != (t * 12)+ 1; count++) {
+    } else if (compound == 1) {
+        for (count = 1; count != (year * 12)+ 1; count++) {
         	A = (A * (r/12)) + parseInt(A);
 			A += parseInt(m) * 1;
 		
@@ -103,8 +97,9 @@ function calculate() {
 }
 
 function calculateGoal() {
+    var goal = parseInt(document.getElementById("goal").value);
     var initial = parseInt(document.getElementById("initial").value);
-    var contribute = parseInt(document.getElementById("contribute").value);
+    var time = parseInt(document.getElementById("time").value);
     var interest = parseInt(document.getElementById("interest").value);
     var compound = parseInt(document.getElementById("compound").value);
 
