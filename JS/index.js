c03cf7fc3031bfead1fa1684ee3a42c75cad211f
TSRGSG <32558834+TSRGSG@users.noreply.github.com>
2018-04-08 15:58:23 -0400

    Added Loan Amortazation

--- JS/index.js
+++ JS/index.js
@@ -23,7 +23,7 @@ function calculate() {
             A += monthly * 12;
 		
 			//Appending to bottom of thet page
-			$(mainList).append("<li>Year " + count + ": $" + addCommas(Math.round(A)) + "</li>");
+			$(mainList).append("<li>Year " + count + ": $" + addCommas(A.toFixed(2)) + "</li>");
 	
 			//Adding to array for graph
 			BottomLabel.push("Year " + count);
@@ -35,7 +35,7 @@ function calculate() {
             A += monthly * 6;
 		
 			//Appending to bottom of thet page
-			$(mainList).append("<li>Semiannually " + count + ": $" + addCommas(Math.round(A)) + "</li>");
+			$(mainList).append("<li>Semiannually " + count + ": $" + addCommas(A.toFixed(2)) + "</li>");
 	
 			//Adding to array for graph
 			BottomLabel.push("Semiannually " + count);
@@ -47,7 +47,7 @@ function calculate() {
             A += monthly * 3;
 		
 			//Appending to bottom of thet page
-			$(mainList).append("<li>Quarter " + count + ": $" + addCommas(Math.round(A)) + "</li>");
+			$(mainList).append("<li>Quarter " + count + ": $" + addCommas(A.toFixed(2)) + "</li>");
 	
 			//Adding to array for graph
 			BottomLabel.push("Quarter " + count);
@@ -59,7 +59,7 @@ function calculate() {
             A += monthly * 1;
 		
 			//Appending to bottom of thet page
-			$(mainList).append("<li>Month " + count + ": $" + addCommas(Math.round(A)) + "</li>");
+			$(mainList).append("<li>Month " + count + ": $" + addCommas(A.toFixed(2)) + "</li>");
 	
 			//Adding to array for graph
 			BottomLabel.push("Month " + count);
@@ -71,7 +71,7 @@ function calculate() {
 	}
 	
 	//Writing to main Total
-    document.getElementById("total").value = "$" + addCommas(Math.round(A));
+	document.getElementById("total").value = "$" + addCommas(A.toFixed(2));
 	
 	//Graphing
 	var ctx = document.getElementById('mainChart').getContext('2d');
@@ -97,26 +97,86 @@ function calculate() {
 }
 
 
-function calculateGoal() {
-    var goal = parseInt(document.getElementById("goal").value);
-    var initial = parseInt(document.getElementById("initial").value);
-    var time = parseInt(document.getElementById("time").value);
+function calculate_loan() {
+    var loan = parseInt(document.getElementById("loan").value);
     var interest = parseInt(document.getElementById("interest").value);
-    var compound = parseInt(document.getElementById("compound").value);
+    var year = parseInt(document.getElementById("year").value);
 
-    //Checking for Yearly, Semiannually, Quarterly, & Monthly.
-    if (compound == 12) {
+	//Getting some variable needed to find monthly payment
+	var i = (interest/100) / 12;
+	var n = 12 * year;
+	
+	//Calculating actual monthly payment amount
+	var discount_factor = ((Math.pow((1 + i), n)) - 1) / (i * Math.pow((1 + i), n));
+	var payment = loan / discount_factor;
 
-    } else if (compound == 6) {
+	//Writing to main Monthly
+	document.getElementById("monthly").value = "$" + addCommas(payment.toFixed(2));
 
-    } else if (compound == 3) {
+	//Getting some variable for loop to print the list
+	var A = loan;
+	var I = 0;
+	var P = 0;
+	var total_interest = 0;
 
-    } else if (compound == 1) {
+	//Cleaning List
+	$(period_list).empty();
+	$(starting_balance_list).empty();
+	$(interest_list).empty();
+	$(principal_list).empty();
+	$(ending_balance_list).empty();
+	$(total_interest_list).empty();
 
-    } else {
-        document.getElementById("total").value = "Compounding can be only 12 (Yearly), 3 (Quarterly), & 1 (Monthly)";
-        //break;
+	//Adding Headers to list
+	$(period_list).append("<li>Month</li>");
+	$(starting_balance_list).append("<li>Starting Balance</li>");
+	$(interest_list).append("<li>Interest</li>");
+	$(principal_list).append("<li>Principal</li>");
+	$(ending_balance_list).append("<li>Ending Balance</li>");
+	$(total_interest_list).append("<li>Total Interest</li>");
+
+
+	//Loop for generating List
+	for (var count = 1; count <= n; count++) {
+		//Appending to period of the page
+		$(period_list).append("<li>" + count + "</li>");
+
+		//Appending to starting balance of the page
+		$(starting_balance_list).append("<li>" + addCommas(A.toFixed(2)) + "</li>");
+
+		//Calculating Interest
+		I = A * i;
+		//Appending to interest list of the page
+		$(interest_list).append("<li>" + addCommas(I.toFixed(2)) + "</li>");
+
+		//Calculating Principal
+		P = payment - I;
+		//Appending to principal list of the page
+		$(principal_list).append("<li>" + addCommas(P.toFixed(2)) + "</li>");
+
+		//Calculatin Ending Balance
+		A = A - P;
+		//Appending to ending balance list of the page
+		$(ending_balance_list).append("<li>" + addCommas(A.toFixed(2)) + "</li>");
+
+		//Calculating for total interest
+		total_interest += I;
+		//Appending to ending balance list of the page
+		$(total_interest_list).append("<li>" + addCommas(total_interest.toFixed(2)) + "</li>");
 	}
+
+}
+
+//Calculator foe finding monthly saving to reach certain goal
+function calculate_goal() {
+	var goal = parseInt(document.getElementById("goal").value);
+	var initial = parseInt(document.getElementById("initial").value);
+	var year = parseInt(document.getElementById("year").value);
+	var interest = parseInt(document.getElementById("interest").value);
+	var compound = parseInt(document.getElementById("compound").value);
+	
+	//Writing to main Total
+	document.getElementById("monthly").value = "Hello World";
 }
 
 //Adding comma to number
